777f6b2716560c7520ea49783c639c5f0e9a14a1
LanceStasinski <lance.stasinski@gmail.com>
2021-06-02 09:52:39 -0400

    chore: Add webpack production config

---
+++ webpack.prod.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+  entry: './src/client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'myLibrary',
+    libraryTarget: 'umd',
+   },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin({}),
+      new TerserPlugin({})
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/client/views/index.html',
+      filename: './index.html'
+    }),
+    new WorkboxPlugin.GenerateSW(),
+    new MiniCssExtractPlugin({filename: '[name].css'})
+  ]
+}
