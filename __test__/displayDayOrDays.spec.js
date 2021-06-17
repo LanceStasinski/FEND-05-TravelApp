76df3665b11bf24a272e6e53ce691a38910f0ec3
LanceStasinski <lance.stasinski@gmail.com>
2021-06-17 18:57:49 -0400

    test: Test displayDayOrDays function

---
+++ __test__/displayDayOrDays.spec.js
@@ -0,0 +1,18 @@
+import { displayDayOrDays } from "../src/client/js/displayDayOrDays";
+
+let data = {}
+const cases = [
+  [data = {daysAway: 1}, '1 Day Away'],
+  [data = {daysAway: 2}, '2 Days Away'],
+  [data = {daysAway: 0}, 'The trip starts today!'],
+]
+
+describe('Display the correct string given daysAway', () => {
+  test.each(cases)(
+    'given data as %p, return %p',
+    (data, expectedResult) => {
+      const result = displayDayOrDays(data);
+      expect(result).toBe(expectedResult);
+    }
+  )
+})
--- src/client/js/displayDayOrDays.js
+++ src/client/js/displayDayOrDays.js
@@ -2,8 +2,12 @@ const displayDayOrDays = (data) => {
   let string = '';
   if (data.daysAway == 1) {
     string  = `1 Day Away`;
+  } else if (data.daysAway == 0){
+    string = `The trip starts today!`;
   } else {
-    string = `${data.daysAway} Days Away`;
+    string = `${data.daysAway} Days Away`
   }
   return string
 }
+
+export { displayDayOrDays }
