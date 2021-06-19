fd9a4c8b94d610fd4bea296b4df19bf3c78230e2
LanceStasinski <lance.stasinski@gmail.com>
2021-06-19 18:09:25 -0400

    feat: Style form

--- src/client/index.js
+++ src/client/index.js
@@ -14,6 +14,7 @@ import { createCountryInfoDiv } from './js/createCountryInfoDiv'
 import './styles/_base.scss'
 import './styles/main.scss'
 import './styles/header.scss'
+import './styles/form.scss'
 
 export {
   addEntry,
