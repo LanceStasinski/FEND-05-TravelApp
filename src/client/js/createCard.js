3167a304514d001f0d2fff59e26f0cd48b88e98c
LanceStasinski <lance.stasinski@gmail.com>
2021-06-21 15:54:47 -0400

    feat: Style card header

--- src/client/js/createCard.js
+++ src/client/js/createCard.js
@@ -5,18 +5,28 @@ const createCard = (data) => {
 
   const header = document.createElement('div');
   header.classList = 'card-header';
+
+  const img = document.createElement('img');
+  img.src = data.imageURL;
+  img.alt = data.imageTag;
+  img.classList = 'city-img';
+  header.appendChild(img);
+
+  const headerText = document.createElement('div');
+  headerText.classList = 'header-text';
   const title = document.createElement('h2');
   title.innerHTML = data.destination;
-  header.appendChild(title);
+  headerText.appendChild(title);
   const daysAway = document.createElement('h3');
   daysAway.innerHTML = Client.displayDayOrDays(data);
-  header.appendChild(daysAway);
+  headerText.appendChild(daysAway);
   const dates = document.createElement('h3');
   const arrival = Client.convertDate(data.arrival);
   const departure = Client.convertDate(data.departure);
   dates.innerHTML = `${arrival} - ${departure}`;
-  header.appendChild(dates);
-  header.style.backgroundImage = `url(${data.imageURL})`;
+  headerText.appendChild(dates);
+  header.appendChild(headerText);
+
   card.appendChild(header);
 
   card.appendChild(Client.createWeatherDiv(data));
