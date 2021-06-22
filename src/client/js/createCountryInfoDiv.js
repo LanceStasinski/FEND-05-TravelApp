6f3304d20f05f86ba55d9a8f833612ac6d2bf929
LanceStasinski <lance.stasinski@gmail.com>
2021-06-22 15:51:18 -0400

    feat: Add styling to country info

--- src/client/js/createCountryInfoDiv.js
+++ src/client/js/createCountryInfoDiv.js
@@ -2,36 +2,36 @@ const createCountryInfoDiv = (data) => {
   const country = document.createElement('div');
   country.classList = 'country';
 
+  const flag = document.createElement('img');
+  flag.src = data.flag;
+  flag.alt = `${data.countryName}'s flag`;
+  flag.classList = 'country-flag';
+  country.appendChild(flag);
+
   const countryInfo = document.createElement('div');
   countryInfo.classList = 'country-info';
 
-  const name = document.createElement('div');
+  const name = document.createElement('h3');
   name.classList = 'country-name';
   name.innerHTML = data.countryName;
   countryInfo.appendChild(name);
 
   const capital = document.createElement('div');
   capital.classList = 'country-capital';
-  capital.innerHTML = `Capital: ${data.capital}`;
+  capital.innerHTML = `<b>Capital</b>: ${data.capital}`;
   countryInfo.appendChild(capital);
 
   const currency = document.createElement('div');
   currency.classList = 'country-currency';
-  currency.innerHTML = `Currency: ${data.currency}`;
+  currency.innerHTML = `<b>Currency</b>: ${data.currency}`;
   countryInfo.appendChild(currency);
 
   const language = document.createElement('div');
   language.classList = 'country-language';
-  language.innerHTML = `Language: ${data.language}`;
+  language.innerHTML = `<b>Language</b>: ${data.language}`;
   countryInfo.appendChild(language);
 
   country.appendChild(countryInfo);
-
-  const flag = document.createElement('img');
-  flag.src = data.flag;
-  flag.alt = `${data.countryName}'s flag`;
-  flag.classList = 'country-flag';
-  country.appendChild(flag);
 
   return country;
 }
