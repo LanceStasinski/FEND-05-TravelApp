de0c00bd69dc6aa6e7749f283c948ed03562a57c
LanceStasinski <lance.stasinski@gmail.com>
2021-06-23 15:10:54 -0400

    feat: Update country info style

--- src/client/index.js
+++ src/client/index.js
@@ -18,7 +18,6 @@ import './styles/footer.scss'
 import './styles/card.scss'
 import './styles/card-header.scss'
 import './styles/country-info.scss'
-import './styles/weather-current.scss'
 import './styles/weather-forecast.scss'
 
 export {
