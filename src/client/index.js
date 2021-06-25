c7f43041b90a4cec1e3273f6d259bb384edccb75
LanceStasinski <lance.stasinski@gmail.com>
2021-06-24 20:15:03 -0400

    feat: Add styling to buttons

--- src/client/index.js
+++ src/client/index.js
@@ -20,6 +20,7 @@ import './styles/card.scss'
 import './styles/card-header.scss'
 import './styles/country-info.scss'
 import './styles/weather-forecast.scss'
+import './styles/buttons.scss'
 
 export {
   addEntry,
--- src/client/js/createButtons.js
+++ src/client/js/createButtons.js
@@ -5,7 +5,7 @@ const createButtons = (data) => {
   buttonHolder.id = `btns-${tripNum}`;
 
   const forecastBtn = document.createElement('button');
-  forecastBtn.classList = 'show-hide';
+  forecastBtn.classList.add('show-hide', 'card-button');
   forecastBtn.innerHTML = 'Forecast';
   forecastBtn.addEventListener('click', () => {
     const weatherDiv = document.getElementById(`weather-${tripNum}`);
@@ -18,8 +18,8 @@ const createButtons = (data) => {
   buttonHolder.appendChild(forecastBtn);
 
   const countryBtn = document.createElement('button');
-  countryBtn.classList = 'show-hide';
-  countryBtn.innerHTML = 'Country Info';
+  countryBtn.classList.add('show-hide', 'card-button');
+  countryBtn.innerHTML = 'Info';
   countryBtn.addEventListener('click', () => {
     const countryDiv = document.getElementById(`country-${tripNum}`);
     if (countryDiv.style.display == 'none') {
@@ -31,12 +31,13 @@ const createButtons = (data) => {
   buttonHolder.appendChild(countryBtn);
 
   const saveBtn = document.createElement('button');
-  saveBtn.classList = 'save';
+  saveBtn.classList.add('save', 'card-button');
   saveBtn.innerHTML = 'Save';
   buttonHolder.appendChild(saveBtn);
   //add event listener
 
   const deleteBtn = document.createElement('button');
+  deleteBtn.classList = 'card-button';
   deleteBtn.innerHTML = 'Delete';
   buttonHolder.appendChild(deleteBtn);
   //add event listener
