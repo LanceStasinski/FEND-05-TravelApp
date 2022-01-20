49edeff8f539a3b2b4385288b496e72acbe5c75d
LanceStasinski <lance.stasinski@gmail.com>
2022-01-20 15:17:04 -0500

    fix: Fix getImageNotUSA so it gives map image if place not found

--- src/server/helpers/getImageNotUSA.js
+++ src/server/helpers/getImageNotUSA.js
@@ -2,18 +2,27 @@ const { getImageRoute } = require("../middleware/getImageRoute");
 
 //If image of the city is not available, get an image of the country.
 const getImageNotUSA = async (coords, key) => {
-  let locationParameters = `${coords.geonames[0].name}+${coords.geonames[0].countryName}`;
-  const city = await getImageRoute(key, locationParameters);
-  console.log(city);
-  let image = "";
+  let image;
   let imageData = [];
-  if (city.total == 0) {
-    locationParameters = `${coords.geonames[0].countryName}`;
-    const country = await getImageRoute(key, locationParameters);
-    image = country;
-  } else {
-    image = city;
+  let locationParameters = `${coords.geonames[0].name}+${coords.geonames[0].countryName}`;
+  try {
+    const city = await getImageRoute(key, locationParameters);
+    if (!city || city.total == 0) {
+      locationParameters = `${coords.geonames[0].countryName}`;
+      const country = await getImageRoute(key, locationParameters);
+      if (!country || country.total == 0) {
+        const map = await getImageRoute(key, "map");
+        image = map;
+      } else {
+        image = country;
+      }
+    } else {
+      image = city;
+    }
+  } catch (error) {
+    console.log(error);
   }
+
   imageData.push(image.hits[0].tags);
   imageData.push(image.hits[0].webformatURL);
   return imageData;
